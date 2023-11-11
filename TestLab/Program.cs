4c517af9dcf331c0238982270f8ac8b03c279ecc
Golubikhin Danila <danila.golubikhin.04@outlook.com>
2023-11-11 16:25:42 +0200

    Patch

--- TestLab/Program.cs
+++ TestLab/Program.cs
@@ -5,23 +5,26 @@ namespace TestLab
     //C# Напишите метод, который получает в качестве параметра введенное целое число большее или равное 10.
     //Метод возвращает число без его крайней левой цифры.
 
-
+    public class Gay
+    {
+        public static int RemoveLeft(int num)
+        {
+            string numstr = num.ToString();
+            string result1 = numstr.Substring(1);
+            int result = int.Parse(result1);
+            return result;
+        }
+    }
     internal class Program
     {
         private static void Main(string[] args)
         {
-            static int RemoveLeft(int num)
-            {
-                string numstr = num.ToString();
-                string result1 = numstr.Substring(1);
-                int result = int.Parse(result1);
-                return result;
-            }
+
         }
     }
 }
 
-//Random random = new Random();
+//Random r = new Random();
 //int input = random.Next(10, 100000);
 //Console.WriteLine("Start number: " + input);
 //int result = RemoveLeft(input);
--- TestLab3Test/UnitTest1.cs
+++ TestLab3Test/UnitTest1.cs
@@ -5,12 +5,13 @@ namespace TestLab3Test
     [TestClass]
     public class UnitTest1
     {
+        Gay gay = new();
         [TestMethod]
         public void TestMethod1()
         {
             int num = 12345;
             int expected = 2345;
-            int result = RemoveLeft(input);
+            int result = Gay.RemoveLeft(num);
             Assert.AreEqual(expected, result);
         }
 
@@ -19,16 +20,32 @@ namespace TestLab3Test
         {
             int num = 78465;
             int expected = 8465;
-            int result = RemoveLeft(input);
+            int result = Gay.RemoveLeft(num);
             Assert.AreEqual(expected, result);
         }
 
         [TestMethod]
         public void TestMethod3()
         {
-            int num = 867878;
-            int expected = 8465;
-            int result = RemoveLeft(input);
+            int num = 5464;
+            int expected = 464;
+            int result = Gay.RemoveLeft(num);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int num = 111;
+            int expected = 11;
+            int result = Gay.RemoveLeft(num);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int num = 23424;
+            int expected = 3424;
+            int result = Gay.RemoveLeft(num);
             Assert.AreEqual(expected, result);
         }
     }
